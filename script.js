f8df4edece1d418017f16d03667e26d1243f98fa
Enya Bullard <enyabullard2@gmail.com>
2025-08-06 22:09:42 -0500

    Add image, set up html, js, css files

---
+++ script.js
@@ -0,0 +1,4 @@
+// This script just logs to the console when the image loads
+document.getElementById("main-image").addEventListener("load", () => {
+  console.log("Image loaded successfully!");
+});
